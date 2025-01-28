1a731d8004a69bcc061dfc4cf6e08b8bc04c4968
Stephen Howe <stevehowe10@gmail.com>
2025-01-27 23:07:53 -0700

    small app

--- old_files/x_backupFolder/tableDrop.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine, text
-
-engine = create_engine('sqlite:///users.db')
-
-with engine.connect() as conn:
-    conn.execute(text('ALTER TABLE contracts ADD COLUMN price_usd FLOAT;'))
-    conn.commit()
-print("Column 'price_usd' added successfully.")
